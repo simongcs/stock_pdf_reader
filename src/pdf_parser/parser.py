27166b0727c3055742e381e4bd91a26b3b18b9f3
simongcs <12417560+simongcs@users.noreply.github.com>
2024-01-23 13:37:10 -0300

    Changed: refactor parser

--- src/pdf_parser/parser.py
+++ src/pdf_parser/parser.py
@@ -3,26 +3,24 @@ from tabula.io import read_pdf
 import re
 import os
 from src.reports.report_model import Report
-from src.stocks.stock_model import Stock
 
 
 class PdfParser:
     reports = set()
     new_pdf_files = set()
-    stocks = set()
+    stocks_dict = dict()
     report_dates = set()
 
     def __init__(self, pdf_path):
         self.pdf_path = pdf_path
 
     def set_stocks(self, stocks):
-        self.stocks = stocks
+        self.stocks_dict = {stock.name: stock for stock in stocks}
 
     def get_unique_report_dates(self, db):
         session = db.get_session()
-        dates = session.query(Report.date).distinct()
+        dates = session.query(Report.date).distinct().all()
         session.close()
-        print("dates:", dates)
         self.report_dates = dates
 
     def pdf_to_df(self, pdf_path) -> DataFrame:
@@ -45,29 +43,39 @@ class PdfParser:
             if file.endswith(".pdf")
         }
         self.new_pdf_files = all_pdf_files - set(
-            [x.date + ".pdf" for x in self.report_dates]
+            [x[0] + ".pdf" for x in self.report_dates]
         )
 
-    def process_files(self, db, stocks: list[Stock]):
-        for pdf_file in self.new_pdf_files:
-            pdf_path = os.path.join(self.pdf_path, pdf_file)
-            filename = os.path.splitext(pdf_file)[0]
-            pdf_date = filename[-8:]
-            df = self.pdf_to_df(pdf_path)
-            reports = [
-                Report(
+    def get_file_data(self, file):
+        pdf_path = os.path.join(self.pdf_path, file)
+        filename = os.path.splitext(file)[0]
+        pdf_date = filename[-8:]
+        return pdf_path, pdf_date
+
+    def create_report(self, data, pdf_date, stock):
+        return Report(
                     pdf_date,
-                    row["Balance (USD)"],
-                    row["Número de acciones"],
-                    (row["Balance (USD)"] / row["Número de acciones"]),
-                    next(
-                        (item.id for item in stocks
-                         if item.name == row["Activo"]), None
-                    ),
+                    data["Balance (USD)"],
+                    data["Número de acciones"],
+                    (data["Balance (USD)"] / data["Número de acciones"]),
+                    stock.id,
                 )
-                for _, row in df.iterrows()
-            ]
-            session = db.get_session()
-            session.add_all(reports)
-            session.commit()
-            session.close()
+
+    def get_reports_from_files(self) -> list[Report]:
+        reports = []
+        for pdf_file in self.new_pdf_files:
+            pdf_path, pdf_date = self.get_file_data(pdf_file)
+            df = self.pdf_to_df(pdf_path)
+            for _, row in df.iterrows():
+                reports.append(
+                    self.create_report(row, pdf_date,
+                                       self.stocks_dict[row["Activo"]])
+                )
+
+        return reports
+
+    def save_reports(self, db, reports):
+        session = db.get_session()
+        session.add_all(reports)
+        session.commit()
+        session.close()
